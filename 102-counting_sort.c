11221fd1f3b0d451825122b066b693b7c24e2ddb
Joseph Ampadu <josephampadu549@gmail.com>
2023-02-17 22:23:39 +0000

    what

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -56,10 +56,10 @@ void counting_sort(int *array, size_t size)
 	for (i = 0; i < (int)size; i++)
 	{
 		j = *(array + i) - 1;
-		*(count + j) += 1;
+		*(count + j) = *(count + j) + 1;
 	}
 	for (i = 1; i <= k; i++)
-		*(count + i) += *(count + i - 1);
+		*(count + i) = *(count + i) + *(count + i - 1);
 	output = malloc(sizeof(int) * size);
 	if (!output)
 	{
@@ -69,7 +69,7 @@ void counting_sort(int *array, size_t size)
 	for (i = size - 1; i >= 0; i--)
 	{
 		j = *(array + i) - 1;
-		*(count + j) -= 1;
+		*(count + j) = *(count + j) - 1;
 		*(output + *(count + j)) = *(array + i);
 	}
 	print_array((const int *)count, k + 1);
