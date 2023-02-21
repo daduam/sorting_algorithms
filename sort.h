5f0aaf1fe66e300e21bb667f1c928b4ad204ea8f
michaelmensa <michaelmensa@github.com>
2023-02-21 15:44:29 +0000

    [UPD] comments, sort.h

--- sort.h
+++ sort.h
@@ -17,13 +17,15 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
-
+/** print array and list functions **/
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+/** mandatory functions **/
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+/** advanced functions **/
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
